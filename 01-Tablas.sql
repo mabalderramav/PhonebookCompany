f5de2d308e6603f5ac78ca9a3716296a4c66b61d
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2016-04-30 20:31:39 -0400

    Se añadio las tablas de usuario,rol y agenda usuario

--- 01-Tablas.sql
+++ 01-Tablas.sql
@@ -19,6 +19,12 @@ CREATE TABLE Agenda (
 )
 ;
 
+CREATE TABLE AgendaUsuario ( 
+	IdAgenda int NOT NULL,
+	IdUsuario int NOT NULL
+)
+;
+
 CREATE TABLE Area ( 
 	IdArea smallint identity(1,1)  NOT NULL,
 	Codigo nvarchar(5) NOT NULL,
@@ -34,9 +40,23 @@ CREATE TABLE Cargo (
 )
 ;
 
+CREATE TABLE Rol ( 
+	IdRol smallint identity(1,1)  NOT NULL,
+	Descripcion nvarchar(100) NOT NULL
+)
+;
+
 CREATE TABLE Ubicacion ( 
 	IdUbicacion smallint identity(1,1)  NOT NULL,
 	Descripcion nvarchar(100) NOT NULL
+)
+;
+
+CREATE TABLE Usuario ( 
+	IdUsuario int identity(1,1)  NOT NULL,
+	Nombre nvarchar(50) NOT NULL,
+	Disponible bit NOT NULL,
+	IdRol smallint NOT NULL
 )
 ;
 
@@ -53,6 +73,14 @@ CREATE INDEX IXFK_Agenda_Ubicacion
 	ON Agenda (IdUbicacion ASC)
 ;
 
+CREATE INDEX IXFK_AgendaUsuario_Usuario
+	ON AgendaUsuario (IdUsuario ASC)
+;
+
+CREATE INDEX IXFK_AgendaUsuario_Agenda
+	ON AgendaUsuario (IdAgenda ASC)
+;
+
 ALTER TABLE Area
 	ADD CONSTRAINT UQ_Area_Codigo UNIQUE (Codigo)
 ;
@@ -65,8 +93,20 @@ ALTER TABLE Cargo
 	ADD CONSTRAINT UQ_Cargo_Codigo UNIQUE (Codigo)
 ;
 
+CREATE INDEX IXFK_Usuario_Rol
+	ON Usuario (IdRol ASC)
+;
+
+ALTER TABLE Usuario
+	ADD CONSTRAINT UQ_Usuario_Nombre UNIQUE (Nombre)
+;
+
 ALTER TABLE Agenda ADD CONSTRAINT PK_Agenda 
 	PRIMARY KEY CLUSTERED (IdAgenda)
+;
+
+ALTER TABLE AgendaUsuario ADD CONSTRAINT PK_AgendaUsuario 
+	PRIMARY KEY CLUSTERED (IdAgenda, IdUsuario)
 ;
 
 ALTER TABLE Area ADD CONSTRAINT PK_Area 
@@ -77,8 +117,16 @@ ALTER TABLE Cargo ADD CONSTRAINT PK_Cargo
 	PRIMARY KEY CLUSTERED (IdCargo)
 ;
 
+ALTER TABLE Rol ADD CONSTRAINT PK_Rol 
+	PRIMARY KEY CLUSTERED (IdRol)
+;
+
 ALTER TABLE Ubicacion ADD CONSTRAINT PK_Ubicacion 
 	PRIMARY KEY CLUSTERED (IdUbicacion)
+;
+
+ALTER TABLE Usuario ADD CONSTRAINT PK_Usuario 
+	PRIMARY KEY CLUSTERED (IdUsuario)
 ;
 
 
@@ -91,6 +139,18 @@ ALTER TABLE Agenda ADD CONSTRAINT FK_Agenda_Ubicacion
 	FOREIGN KEY (IdUbicacion) REFERENCES Ubicacion (IdUbicacion)
 ;
 
+ALTER TABLE AgendaUsuario ADD CONSTRAINT FK_AgendaUsuario_Usuario 
+	FOREIGN KEY (IdUsuario) REFERENCES Usuario (IdUsuario)
+;
+
+ALTER TABLE AgendaUsuario ADD CONSTRAINT FK_AgendaUsuario_Agenda 
+	FOREIGN KEY (IdAgenda) REFERENCES Agenda (IdAgenda)
+;
+
 ALTER TABLE Cargo ADD CONSTRAINT FK_Cargo_Area 
 	FOREIGN KEY (IdArea) REFERENCES Area (IdArea)
+;
+
+ALTER TABLE Usuario ADD CONSTRAINT FK_Usuario_Rol 
+	FOREIGN KEY (IdRol) REFERENCES Rol (IdRol)
 ;
